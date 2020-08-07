091ce133106d0994b334148a0ddf19021acd2a2f
Daniel Ma <ziyuema@gmail.com>
2020-08-07 16:06:59 -0400

    Formatting finshed

--- server.js
+++ server.js
@@ -3,18 +3,27 @@
 const express = require("express");
 const http = require("http");
 const path = require("path");
+const livereload = require("livereload");
+let connectLivereload = require("connect-livereload");
 
-const updater = require("./lib/server/updater");
+let publicPath = path.join(__dirname, "public");
 
 const PORT = 8080;
 
 let app = express();
 let server = http.createServer(app);
 
-let publicPath = path.join(__dirname, "src");
-app.use("/lib/client", express.static(path.join(__dirname, "lib/client")));
+let liveReloadServer = livereload.createServer();
+liveReloadServer.watch(publicPath);
+
+liveReloadServer.server.once("connection", () => {
+    setTimeout(() => {
+        liveReloadServer.refresh("/");
+    }, 100);
+});
+
+app.use(connectLivereload());
 app.use(express.static(publicPath));
-updater(server, publicPath);
 
 server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}.`);
